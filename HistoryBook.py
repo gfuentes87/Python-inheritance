1075fb9176c1d23e420686f90a08f2e7ccc32702
gfuentes87 <gustavo.fuentes87@gmail.com>
2014-09-13 16:19:54 -0300

    Create HistoryBook.py

---
+++ HistoryBook.py
@@ -0,0 +1,16 @@
+from Book import Book
+'''
+Created on Sep 13, 2014
+
+@author: syd
+'''
+
+class HistoryBook(Book):
+    
+    def __init__(self, name, author, year):
+        Book.__init__(self, name, author)
+        self.year = year
+        
+    
+    def displayCompleteInfo(self):
+        print Book.getBasicInformation(self)+", Year: "+str(self.year)
